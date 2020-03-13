bb43e2c90174ec747f5748d1ec2d753997c77e17
JKamlah <>
2020-03-13 16:58:44 +0100

    ADD working with merge-conflict files, improved diffhead calculation, fixed symlink for images

--- gtcheck/app.py
+++ gtcheck/app.py
@@ -38,23 +38,27 @@ def color_diffs(difftext):
         .replace("[-",'<span style="color:red">')\
         .replace("-]","</span>")
 
-def surrounding_images(folder,img):
-    prev_img, post_img = "", ""
-    imgnr = "".join([char for char in img.name if char.isdigit()])
-    imgint = int(imgnr)
-    imgprefix = img.name.split(".", 1)[0].replace(imgnr, "")
-    if imgint > 0:
-        prev_imgs = list(folder.joinpath(imgprefix).glob("*"+str(imgint-1)+f"*.{img.name.split('.',1)[1]}"))
-        if prev_imgs:
-            prev_img = prev_imgs[0]
-    post_imgs = list(folder.joinpath(imgprefix).glob("*" + str(imgint + 1) + f"*.{img.name.split('.', 1)[1]}"))
-    if post_imgs:
-        post_img = post_imgs[0]
+def surrounding_images(img, imgfolder, folder):
+    import re
+    imgmatch = re.match(r"^(.*?)(\d+)(\D*)$",img.name)
+    imgint = int(imgmatch[2])
+    imgprefix = img.name[:imgmatch.regs[1][1]]
+    imgpostfix = img.name[imgmatch.regs[3][0]:]
+    prev_img = img.parent.joinpath(imgprefix+str(imgint-1)+imgpostfix)
+    post_img = img.parent.joinpath(imgprefix + str(imgint + 1) + imgpostfix)
+    if prev_img.exists():
+        prev_img = Path("./symlink/").joinpath(prev_img.relative_to(folder.parent))
+    else:
+        prev_img = None
+    if post_img.exists():
+        post_img = Path("./symlink/").joinpath(post_img.relative_to(folder.parent))
+    else:
+        post_img = None
     return prev_img, post_img
 
 @lru_cache(maxsize=None)
 def get_repo(path):
-    return Repo(path)
+    return Repo(path, search_parent_directories=True)
 
 @app.route("/gtcheck", methods=["GET", "POST"])
 def gtcheck():
@@ -64,15 +68,30 @@ def gtcheck():
     if name == "":
         name = "GTChecker"
     email = repo.config_reader().get_value("user", "email")
+    # Diff Head
+    diffhead = repo.git.diff('--cached','--shortstat').strip().split(" ")[0]
     # untracked files to potential add
     [repo.git.add("-N", item) for item in repo.untracked_files if ".gt.txt" in item]
     difflist = [item for item in repo.index.diff(None, create_patch=True, word_diff_regex=".") if ".gt.txt" in "".join(Path(item.a_path).suffixes)]
-    for diffidx, item in enumerate(difflist):
+    mergelist =  [item for item in repo.index.diff(None) if ".gt.txt" in "".join(Path(item.a_path).suffixes)]
+    for diffidx, item in enumerate(difflist+mergelist):
         if diffidx < session["skip"]: continue
+        if not item.a_blob and not item.b_blob: continue
         session['modtype'] = "mod"
-        difftext = "".join(item.diff.decode('utf-8').split("\n")[1:])
-        diffcolored = color_diffs(difftext)
+        mergetext = []
         origtext = item.a_blob.data_stream.read().decode('utf-8').strip("\n ")
+        if "<<<<<<< HEAD\n" in origtext:
+            with open(folder.joinpath(item.a_path),"r") as fin:
+                mergetext = fin.read().split("<<<<<<< HEAD\n")[-1].split("\n>>>>>>>")[0].split("\n=======\n")
+                from subprocess import run, PIPE
+                p = run(['git','hash-object','-','--stdin'], stdout=PIPE,
+                        input=mergetext[0], encoding='utf-8')
+                p2 = run(['git','hash-object','-','--stdin'], stdout=PIPE,
+                        input=mergetext[1], encoding='utf-8')
+                difftext = repo.git.diff(p.stdout.strip(),p2.stdout.strip(),"-p","--word-diff").split("@@")[-1].strip()
+        else:
+            difftext = "".join(item.diff.decode('utf-8').split("\n")[1:])
+        diffcolored = color_diffs(difftext)
         if origtext == "" and not item.deleted_file or item.new_file:
             session['modtype'] = "new"
             diffcolored = "<span style='color:green'>This untracked file gets added when committed and deleted when stashed!</span>"
@@ -80,44 +99,43 @@ def gtcheck():
             session['modtype'] = "del"
             modtext = ""
             diffcolored = "<span style='color:red'>This file gets deleted when committed and restored when stashed!</span>"
+        elif mergetext:
+            session['modtype'] = "merge"
+            modtext = mergetext[1]
         else:
             modtext = folder.absolute().joinpath(item.b_path).open().read().strip("\n ")
 
         fname = folder.joinpath(item.a_path)
         mods = modifications(difftext)
-        if repo.index.diff('HEAD'):
-            commitmsg = f"[GT Checked] Staged Files: {len(repo.index.diff('HEAD'))}"
+        if diffhead:
+            commitmsg = f"[GT Checked] Staged Files: {diffhead}"
         else:
             commitmsg = f"[GT Checked]  {item.a_path}: {', '.join([orig + ' -> ' + mod for orig, mod in mods])}"
         session['modtext'] = modtext
         session['fname'] = str(fname)
         session['fpath'] = str(item.a_path)
         session['diffidx'] = diffidx
-        imgfolder = Path(__file__).resolve().parent.joinpath(f"static/symlink/{fname.parent.name}")
+
+        imgfolder = Path(__file__).resolve().parent.joinpath(f"static/symlink/{folder.name}")
         # Create symlink to imagefolder
         if not imgfolder.exists():
-            print(imgfolder)
-            imgfolder.symlink_to(fname.parent.absolute())
-        inames = [iname for iname in fname.parent.glob(f"{fname.name.split('.')[0]}*") if imghdr.what(iname)]
+            #print(imgfolder)
+            imgfolder.symlink_to(folder)
+        inames = [iname for iname in fname.parent.glob(f"{fname.name.replace('.gt.txt','')}*") if imghdr.what(iname)]
         img = inames[0] if inames else None
         if not img:
             return render_template("gtcheck.html", repo=session["folder"], branch=repo.active_branch, name=name, email=email ,commitmsg=commitmsg,
                                    difftext=Markup(diffcolored), origtext=origtext, modtext=modtext, files_left=str(len(difflist)),
                                    iname="No image", fname=str(fname.name), skipped=session['skip'])
         else:
-            img_out = str(imgfolder.joinpath(img.name)).split("static")[-1][1:]
-            prev_img, post_img = surrounding_images(fname.parent, img)
-            if not isinstance(prev_img, str):
-                prev_img = str(imgfolder.joinpath(prev_img.name)).split("static")[-1][1:]
-            if not isinstance(post_img, str):
-                post_img = str(imgfolder.joinpath(post_img.name)).split("static")[-1][1:]
-
+            img_out = Path("./symlink/").joinpath(img.relative_to(folder.parent))
+            prev_img, post_img = surrounding_images(img, imgfolder, folder)
             return render_template("gtcheck.html", repo=session["folder"], branch=repo.active_branch, name=name, email=email, commitmsg=commitmsg, image=img_out, previmage=prev_img, postimage=post_img,
                                difftext=Markup(diffcolored), origtext=origtext, modtext=modtext, files_left=str(len(difflist)),
                                iname=str(img.name), fname=str(fname.name), skipped=session['skip'])
     else:
-        if repo.index.diff('HEAD'):
-            commitmsg = f"[GT Checked] Staged Files: {len(repo.index.diff('HEAD'))}"
+        if diffhead:
+            commitmsg = f"[GT Checked] Staged Files: {len(diffhead)}"
             modtext = f"Please commit the staged files! You skipped {session['skip']} files."
             return render_template("gtcheck.html", name=name, email=email, commitmsg=commitmsg, modtext=modtext, files_left="0")
         if not difflist:
@@ -131,7 +149,7 @@ def gtcheckedit():
     fname = Path(session["folder"]).joinpath(session['fpath'])
     data = request.form #.to_dict(flat=False)
     if data['selection'] == 'commit':
-        if session['modtext'] != data['modtext']:
+        if session['modtext'] != data['modtext'] or session['modtype'] == "merge":
             with open(fname, "w") as fout:
                 fout.write(data['modtext'])
         repo.git.add(str(fname),u=True)
@@ -156,16 +174,15 @@ def gtcheckinit():
     repo = get_repo(folder)
     session["folder"] = folder
     session["skip"] = 0
-
     if data["branches"] != repo.active_branch:
         assert repo.untracked_files, "Untracked files detected, please resolve for checkout branches"
-        repo.git.checkout(data["branches"])
+        #repo.git.checkout(data["branches"])
     if data.get("checkout","off") == "on" and data["new_branch"] != "":
         assert repo.untracked_files, "Untracked files detected, please resolve for checkout branches"
         repo.git.checkout(data["checkout"], b=data["new_branch"])
         # Check requirements
     assert not repo.bare, "Git repo is bare"  # check if repo is bare
-    assert repo.is_dirty(), "No modified gt-files in the repo"  # check the dirty state
+    assert repo.is_dirty(), "No modified gt-files in the repository"  # check the dirty state
     return gtcheck()
 
 
@@ -182,7 +199,8 @@ def index():
         folder = Path(sys.argv[1])
     else:
         folder = Path(".")
-    repo = Repo(folder)
+    repo = get_repo(folder)
+    folder = Path(repo.git_dir).parent
     name = repo.config_reader().get_value("user", "name")
     clean_symlinks()
     if name == "":
@@ -215,7 +233,7 @@ if not app.debug:
 def run():
     port = int(os.environ.get('PORT', 5000))
     app.config['SECRET_KEY'] = str(int(time.time()))
-    #webbrowser.open_new('http://127.0.0.1:5000/')
+    webbrowser.open_new('http://127.0.0.1:5000/')
     app.run(host='127.0.0.1', port=port, debug=True)
 
 if __name__ == "__main__":
