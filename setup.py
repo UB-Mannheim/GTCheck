804bbc888687350387b4132089bff3044fcd81cb
JKamlah <jan.kamlah@gmail.com>
2021-08-19 11:05:38 +0200

    Add setup script.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='gtcheck',
-      version='0.2',
+      version='0.5',
       description='Check modified gt line files in git repos',
       author='Jan Kamlah',
       url='https://github.com/jkamlah/gtcheck/',
@@ -18,7 +18,7 @@ setup(name='gtcheck',
           'gitpython'
       ],
       include_package_data=True,
-      package_data = {'gtcheck': ['gtcheck/static/*', 'gtcheck/templates/*']},
+      package_data = {'gtcheck': ['gtcheck/static/*', 'gtcheck/templates/*', '*.py']},
       classifiers=[
           'Environment :: Console',
           'Environment :: Web Environment',
